1b38e719b77b40b8f9566f57ad20dd9e908a9786
Samuel Watson <otacon100@hotmail.com>
2018-06-23 16:04:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
-
 function shout(string) {
   return string.toUpperCase()
 }
@@ -26,8 +12,4 @@ function logShout(string) {
 
 function logWhisper(string) {
   console.log(string.toLowerCase())
-}
-
-function sayHiToGrandma(string) {
-  return string()
 }
