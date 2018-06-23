2cc18ae826bb9da93c63e2c8d154fcc45615e7cc
Samuel Watson <otacon100@hotmail.com>
2018-06-23 17:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === 'I love you, grandma.') return ("I love you, too.");
+  if (string === 'I love you, Grandma.') return ("I love you, too.");
   else if (string === uppercase) return ("YES INDEED!");
   else return ("I can't hear you!")
 }
